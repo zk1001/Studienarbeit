df18d83847b2f3902b52ab0c174d5df4d7782680
dexter2406 <46349448+dexter2406@users.noreply.github.com>
2020-01-17 17:23:14 +0100

    batchTest_EP

--- imcra_EPbased/batch_test_EP.m
+++ imcra_EPbased/batch_test_EP.m
@@ -1,62 +1,43 @@
+%% EP_based noise estimation comparison
 
-addpath('D:\Stud\Studienarbeit\TestFiles\SpeechMaterial\0\LGF')
-addpath('D:\Stud\Studienarbeit\TestFiles\SpeechMaterial\3\LGF')
-addpath('D:\Stud\Studienarbeit\TestFiles\SpeechMaterial\5\LGF')
-addpath('D:\Stud\Studienarbeit\TestFiles\SpeechMaterial\7\LGF')
-%%
-m = 0;
-figure;
-correct_rate_set = [];
-for i = 2:2:6
-    j = 100+i;
-    file_str = strcat('HSMm0',num2str(j));
-    m = m+1;
-    subplot(3,1,m)
-    inst_SNR_set=[];
+SNR_set = [];
+phat_set = [];
+for i = 0:2
+    yinpath = strcat('D:\Stud\Studienarbeit\TestFiles\SpeechMaterial\',num2str(i),'\LGF');
+    addpath(yinpath)
+    yinFile = strcat('HSMm0103_snr=',num2str(i),'.mat');
     
-    for k = [0,3,5]
-        filename = strcat(file_str,'_snr=',num2str(k),'.mat');
-        eta_set = imcra_EPbased(filename);
-        inst_SNR = sum(eta_set);
-        
-        inst_SNR_set = [inst_SNR_set; inst_SNR];
-        
-        hold on
-        plot(inst_SNR);
-    end
-    legend('0','3','5');
-    title('instant SNR estimated, real=0,3,5');
-    hold off
-    
-    % calculate correct rate of each file between different noise level.
-    % each column is a different file; each row is comparison between
-    % different SNR.
-    correct_rate = criteria(inst_SNR_set);
-    correct_rate_set = [correct_rate_set correct_rate];
+    [snrs_div, snrs_log , phat] = imcra_EPbased(yinFile);
+    SNR_set = [SNR_set; snrs_log];
+    phat_set = [phat_set; sum(phat)];
 end
-%%
-addpath('D:\Stud\Studienarbeit\TestFiles\SpeechMaterial\0')
-addpath('D:\Stud\Studienarbeit\TestFiles\SpeechMaterial\3')
-addpath('D:\Stud\Studienarbeit\TestFiles\SpeechMaterial\5')
-addpath('D:\Stud\Studienarbeit\TestFiles\SpeechMaterial\7')
-m = 0;
-figure;
-for i = 2:2:6
-    j = 100+i;
-    file_str = strcat('HSMm0',num2str(j));
-    
-    m = m+1;
-    subplot(3,1,m)
-    
-    for k = [0,3,5,7]
-        filename = strcat(file_str,'_snr=',num2str(k),'.wav');
-        [~, ~, ~,eta_set] = correct_loop_mode(filename);
-        SNR_eta = sum(eta_set);
+% Plotting
 
-        hold on
-        plot(SNR_eta);
-    end
-    legend('0','3','5','7');
-    title('instant SNR estimated, real=0,3,5,7');
-    hold off   
+figure;
+% compare snr=0 vs. snr=1 for example
+
+for j = [1,3]
+    hold on
+%     subplot(211);plot(SNR_set(j,:));
+    plot(SNR_set(j,:),'LineWidth',1.2);
+
 end
+% subplot(211);plot(zeros(size(SNR_set,2),1));
+plot(zeros(size(SNR_set,2),1),'LineWidth',0.8);
+ylabel('SNR(dB)','Fontsize',30)
+xlabel('Frames','Fontsize',30)
+legend('realSNR=0','realSNR=2','0dB line','Fontsize',20);
+title('EP-based instSNR compare, 0vs2, "CCITT"','Fontsize',30)
+ylim([-20,20]);
+hold off
+
+% Using SPP
+% for j = [1,3]
+%     % Use a step of 2 to "smooth" the curve,
+%     %by picking max out of every 2 samples
+%     hold on
+%     plot(phat_set(j,:)/8);
+% end
+% legend('realSNR=0','realSNR=2')
+% title('SPP')
+% hold off
