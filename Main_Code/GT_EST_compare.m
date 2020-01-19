a456d94d6250c866212ef793d38b4bf4fe31aea8
dexter2406 <46349448+dexter2406@users.noreply.github.com>
2020-01-19 11:28:19 +0100

    add 'find_threshold' and 'cor_rate'

--- Main_Code/GT_EST_compare.m
+++ Main_Code/GT_EST_compare.m
@@ -6,27 +6,29 @@
 
 %% just for testing
 % This part might be pre-processed outside the function
-addpath('D:\Stud\Studienarbeit\Code_IMCRA\IMCRA_algorithm')
 addpath('D:\Stud\Studienarbeit\Code_MMSE_BC\MMSE_BC_SW')
 addpath('D:\Stud\Studienarbeit\Code_MMSE_SSP\MMSE_SSP_SW')
 addpath('D:\Stud\Studienarbeit\TestFiles\Source\HSM')
 addpath('D:\Stud\Studienarbeit\TestFiles\Source\Noise')
 %addpath('D:\Stud\Studienarbeit\TestFiles\SpeechMaterial\5')
-
-%% Noisy sythesis
+%%
 snr = 2;
-[x_, d_, y_, fs] = noisy_gen(3,'HSM','CCITT', 90, 45);
-% Normalization
-for i = 1:size(y_,1)
-    y_in(i,:) = y_(i,:) / max(abs(y_(i,:)));
-    x_in(i,:) = x_(i,:) / max(abs(y_(i,:)));
-    d_in(i,:) = d_(i,:) / max(abs(y_(i,:)));
-end
+[x_, d_, y_, fs] = noisy_gen(snr,'HSM','CCITT',90, 45);
+y_in = y_;
+x_in = x_;
+d_in = d_;
+% Normalization for stereo
+% for i = 1:size(y_,1)
+%     y_in(i,:) = y_(i,:) / max(abs(y_(i,:)));
+%     x_in(i,:) = x_(i,:) / max(abs(y_(i,:)));
+%     d_in(i,:) = d_(i,:) / max(abs(y_(i,:)));
+% end
 
 %% FOR MMSE_SPP
+% [estSNR_set,~] = mmse_spp_audio(y_in'); %single-channel
 trueSNR_set = GT_stereo(x_in, d_in, 'mmse_spp');
 for i = [1,2]
-    [estSNR_set(:,i),~] = mmse_spp_audio(y_in(i,:)',fs);
+    [estSNR_set(i,:),~] = mmse_spp_audio(y_in(i,:)');
 end
 
 % Plotting Ground Truth of respective ears
@@ -34,84 +36,94 @@ figure;
 N = length(trueSNR_set);
 subplot(2,1,1);
 for i = [1,2]
-    hold on; plot(1:N, trueSNR_set(:,i));
+    hold on; plot(1:N, trueSNR_set(i,:));
     plot(zeros(1,N));
-    ylim([-20, 20]);
+    ylim([-15, 20]);
     hold off
 end
 xlabel('frame'); ylabel('snr(dB)')
 legend('left','right', '0dB')
-text = strcat('Ground Truth - respective ears, snr=',num2str(snr),...
+titel_text = strcat('Ground Truth - respective ears, snr=',num2str(snr),...
         ', MMSE-SPP');
-title(text)
+title(titel_text)
 
 % Plotting Estimation results
 subplot(2,1,2)
 for i = [1,2]
-    hold on; plot(1:N, estSNR_set(:,i));
+    hold on; plot(1:N, estSNR_set(i,:));
     plot(zeros(1,N));
-    ylim([-20, 20]);
+    ylim([-15, 20]);
     hold off
 end
 xlabel('frame'); ylabel('snr(dB)')
 legend('left','right', '0dB')
-text = strcat('Estimation - respective ears, snr=',num2str(snr),...
+titel_text = strcat('Estimation - respective ears, snr=',num2str(snr),...
     ', MMSE-SPP');
-title(text)
+title(titel_text)
 
 %%%%%%%%%% Statistical Evaluation
 % 1.Mean of effective segments
-% Select effective segements by finding the frame with snr > 0
-idx_pos_true1= find(trueSNR_set(:,1) > 0);
-idx_pos_true2= find(trueSNR_set(:,2) > 0);
-idx_pos_esti1= find(estSNR_set(:,1) > 0);
-idx_pos_esti2= find(estSNR_set(:,2) > 0);
-
-
-% Mean of SNRs which are > 0 dB (segmental SNR)
-trueSNR_eff1 = trueSNR_set(:,1);
-trueSNR_eff1(trueSNR_eff1<0) = 0;
-trueSNR_eff2 = trueSNR_set(:,2);
-trueSNR_eff2(trueSNR_eff2<0) = 0;
-trueSNR_eff = [trueSNR_eff1, trueSNR_eff2];
-
-estiSNR_eff1 = estSNR_set(:,1);
-estiSNR_eff1(estiSNR_eff1<0) = 0;
-estiSNR_eff2 = estSNR_set(:,2);
-estiSNR_eff2(estiSNR_eff2<0) = 0;
-estiSNR_eff = [estiSNR_eff1, estiSNR_eff2];
-
-mean_true = mean(trueSNR_eff);
-mean_esti = mean(estiSNR_eff);
+% mean_SNR output:
+% - mean of only speech-present segments (SNR>=threshold)
+% - effective SNR set (if SNR<threhold, then set to equal to threshold)
+thresh = 0;
+[mean_true, effSNR_true] = mean_SNR(thresh, trueSNR_set);
+[mean_esti, effSNR_esti] = mean_SNR(thresh, estSNR_set);
 
 fprintf('true mean of left ear: %f dB\n',mean_true(1))
 fprintf('true mean of right ear: %f dB\n',mean_true(2))
-
 fprintf('estimated mean of left ear: %f dB\n',mean_esti(1))
 fprintf('estimated mean of right ear: %f dB\n',mean_esti(2))
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % 2.Root-Mean-Square-Error (RMSE) of ground truth and estimate
-
-rmse_true = sqrt( mean( (trueSNR_set(:,1) - trueSNR_set(:,2)).^2 ) );
-rmse_esti = sqrt( mean( (estSNR_set(:,1) - estSNR_set(:,2)).^2 ) );
+rmse_true = sqrt( mean( (effSNR_true(1,:) - effSNR_true(2,:)).^2 ) );
+rmse_esti = sqrt( mean( (effSNR_esti(1,:) - effSNR_esti(2,:)).^2 ) );
 
 fprintf('RMSE of the Ground truth between two ears: %f\n', rmse_true)
 fprintf('RMSE of the Estimation between two ears: %f\n', rmse_esti)
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% 
-% 2. Correct rate
+% 3. Correct rate
 % Regardless of the values, just telling which ear has the better SNR
-% NOTE: Estimation has "delays"
+% NOTE: would be tricky if Estimation has "delays"
+[correct_rate] = cor_rate(effSNR_true, effSNR_esti);
+fprintf('correct rate: %f',correct_rate)
+%% Find the best threshold
+rmse_set = [];
+mean_set = [];
+thresh = -10:0.5:1;
+for i = thresh
+    [criteria_rmse, criteria_mean]  = find_best_thresh(i,trueSNR_set,estSNR_set);
+    rmse_set = [rmse_set, criteria_rmse];
+    mean_set = [mean_set, criteria_mean];
+    [min_rmse, idx_rmse] = min(rmse_set);
+    [min_mean, idx_mean] = min(mean_set);
+    thresh_rmse = thresh(idx_rmse);
+    thresh_mean = thresh(idx_mean);
+end
+fprintf('the best threshold based on rmse is %f\n', thresh(idx_rmse))
+fprintf('the best threshold based on mean is %f', thresh(idx_mean))
+figure; 
+plot(1:length(thresh),[rmse_set;mean_set])
+str1 = strcat('\leftarrow threshold: ',num2str(thresh_rmse));
+str2 = strcat('\leftarrow threshold: ',num2str(thresh_mean));
+text(idx_rmse,rmse_set(idx_rmse),str1)
+text(idx_mean,mean_set(idx_mean),str2)
+xlabel('threshold candidates')
+ylabel('difference(GT vs. EST)')
+legend('mean\_difference','rmse\_difference')
 
 
-
-%% FOR IMCRA
+%% IMCRA_SW (single-channel audio)
 % ADD_NOISE takes in audios and snr
 % output are 
 % - trimmed noise audios with the same length as speech
 % - synthesized noisy speech with desired snr
+addpath('D:\Stud\Studienarbeit\Code_IMCRA')
 snr = 2;
+[x_in,fs1] = audioread('HSMm0103.wav');
+[d_orig] = audioread('CCITT.wav');
 [d_in, y_in] = add_noise(x_in, d_orig, snr);
 y_in = y_in/max(abs(y_in));
 X_pow = stft_imcra(x_in,fs1);
